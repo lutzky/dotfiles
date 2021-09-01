af9f2f52e1edb9f512e79063e9f38db911bf5be9
Ohad Lutzky <ohad@lutzky.net>
2021-09-01 11:14:26 +0100

    Add instructions about fortunes_url

--- stow-fortunes/.fortunes/update_fortunes.sh
+++ stow-fortunes/.fortunes/update_fortunes.sh
@@ -4,6 +4,8 @@ set -e
 
 cd $(dirname $0)
 
+# For the fortunes_url link in Sheets: Go to "File -> Publish to the web",
+# Change the type to "comma-separate values" (and publish the doc)
 URL="$(cat fortunes_url)"
 
 curl -sS -L "$URL" | awk '{print;print "%"}' > fortunes
