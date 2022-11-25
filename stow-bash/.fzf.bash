8799ec308b25c58e2b2dadcfc0fb2bb212928ab3
Ohad Lutzky <ohad@lutzky.net>
2022-11-25 22:30:33 +0000

    Restore .fzf.bash

---
+++ stow-bash/.fzf.bash
@@ -0,0 +1,9 @@
+# https://github.com/junegunn/fzf
+
+# Install Ctrl-R (history search), Ctrl-T (file search), Alt-C (cd)
+source /usr/share/doc/fzf/examples/key-bindings.bash 
+
+# Fuzzy-find when using ** and tab
+# (Should be auto-installed, but doesn't seem to work 🤷)
+source /usr/share/bash-completion/completions/fzf
+
