2d9db4700695417093590abe7600d49b42d5bb90
Ohad Lutzky <lutzky@google.com>
2023-02-17 14:32:04 +0000

    bash-fzf: Alternative file locations

--- stow-bash/.fzf.bash
+++ stow-bash/.fzf.bash
@@ -1,9 +1,14 @@
 # https://github.com/junegunn/fzf
 
 # Install Ctrl-R (history search), Ctrl-T (file search), Alt-C (cd)
-source /usr/share/doc/fzf/examples/key-bindings.bash 
+source /usr/share/doc/fzf/examples/key-bindings.bash
 
 # Fuzzy-find when using ** and tab
 # (Should be auto-installed, but doesn't seem to work 🤷)
-source /usr/share/bash-completion/completions/fzf
-
+if [[ -e /usr/share/bash-completion/completions/fzf ]]; then
+	source /usr/share/bash-completion/completions/fzf
+elif [[ -e /usr/share/doc/fzf/examples/completion.bash ]]; then
+	source /usr/share/doc/fzf/examples/completion.bash;
+else
+	echo "WARNING: $BASH_SOURCE:$LINENO: Could not load fzf completions"
+fi
