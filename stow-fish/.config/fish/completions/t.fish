ad91a3c08fc4fa9a4a5f12a3035427f47c55cdbe
Ohad Lutzky <ohad@lutzky.net>
2024-04-08 20:53:11 +0100

    stow-fish: Better t completion

--- stow-fish/.config/fish/completions/t.fish
+++ stow-fish/.config/fish/completions/t.fish
@@ -1,1 +1,2 @@
-complete -c t --no-files -a "(tmux has-session 2> /dev/null && tmux ls -F '#{session_name}')"
+complete -c t -d 'Session name' -xa "(tmux has-session 2> /dev/null && tmux ls -F '#{session_name}')" -n '__fish_is_first_token'
+complete -c t -xa ''
