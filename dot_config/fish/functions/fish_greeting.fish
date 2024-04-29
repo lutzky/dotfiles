7eb9d5ee82b34304e01d2af9ee4aa8c1b6389254
Ohad Lutzky <ohad@lutzky.net>
2024-04-29 12:57:17 +0100

    fish_greeting: Move personal greeting to end

--- dot_config/fish/functions/fish_greeting.fish
+++ dot_config/fish/functions/fish_greeting.fish
@@ -1,4 +1,15 @@
 function fish_greeting
+    if [ -z $TMUX ]
+        echo "Active tmux sessions: (remember t, C-a w)"
+        if tmux has-session > /dev/null 2>&1
+            tmux ls
+        else
+            echo "(none)"
+        end
+    end
+
+    type -q _fish_greeting_work && _fish_greeting_work
+
     if type -q bidiv
         set rtl_fixer bidiv -j
     else
@@ -10,15 +21,4 @@ function fish_greeting
     else
         echo "~/.fortunes not configured"
     end
-
-    if [ -z $TMUX ]
-        echo "Active tmux sessions: (remember t, C-a w)"
-        if tmux has-session > /dev/null 2>&1
-            tmux ls
-        else
-            echo "(none)"
-        end
-    end
-
-    type -q _fish_greeting_work && _fish_greeting_work
 end
