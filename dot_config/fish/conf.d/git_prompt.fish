3edd706817db7509b24f5bfd045920f78249fc56
Ohad Lutzky <ohad@lutzky.net>
2024-05-03 12:53:43 +0100

    fish: Use more compatible git symbols

--- dot_config/fish/conf.d/git_prompt.fish
+++ dot_config/fish/conf.d/git_prompt.fish
@@ -1,2 +1,9 @@
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_showcolorhints 1
+
+# Avoid wide characters
+set -g __fish_git_prompt_char_cleanstate   '✓' # Default: ✔ 
+set -g __fish_git_prompt_char_dirtystate   '🞢' # Default: ✚ 
+set -g __fish_git_prompt_char_invalidstate '✕' # Default: ✖
+set -g __fish_git_prompt_char_stashstate   '⚑' # Default: ⚑
+set -g __fish_git_prompt_char_stagedstate  '●' # Default: ●
