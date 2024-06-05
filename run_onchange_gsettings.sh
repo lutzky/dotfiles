a95acd25cc29f41e280eb30a6f366bee02f452ba
Ohad Lutzky <ohad@lutzky.net>
2024-06-05 17:04:34 +0100

    gsettings: Don't error on missing schema

--- run_onchange_gsettings.sh
+++ run_onchange_gsettings.sh
@@ -2,5 +2,15 @@
 
 # Don't set -e, let things fail loudly but continue if there's an issue.
 
+set_or_warn() {
+  if gsettings list-keys $1 > /dev/null 2>&1; then
+    gsettings set $1 $2 $3
+  else
+    echo "WARN: Skipping due to missing schema: gsettings set $1 $2 $3"
+  fi
+}
+
+echo "INFO: Running gsettings commands"
+
 # https://askubuntu.com/a/1297664
-gsettings set org.gnome.evolution-data-server.calendar notify-with-tray true
+set_or_warn org.gnome.evolution-data-server.calendar notify-with-tray true
