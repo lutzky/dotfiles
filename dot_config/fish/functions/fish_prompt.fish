38fc5c5abe5754ef136e4ac53b3b6358f11baafe
Ohad Lutzky <ohad@lutzky.net>
2024-06-30 13:27:58 +0100

    Prompt: Unify on left, standardize

--- dot_config/fish/functions/fish_prompt.fish
+++ dot_config/fish/functions/fish_prompt.fish
@@ -1,12 +1,23 @@
 function fish_prompt
+    # This prompt is designed to be ocassionally copied-and-pasted. For this
+    # reason, we:
+    #
+    # * Avoid fish_right_prompt - this is on the same line as the actual
+    #   command (multiple lines are not supported). It also makes resizing a
+    #   bit ugly.
+    # * Use $ and # in the prompt instead of prettier characters. This is
+    #   sometimes treated specially by "copy this commandline" doc renderers.
+
     set -l cmd_status $status
+    set -l duration (__human_duration $CMD_DURATION)
 
     if type -q _fish_prompt.work
         _fish_prompt.work
-        echo -n (set_color -r $fish_color_host_remote)' '(set_color normal)
+        echo -n (set_color $fish_color_quote)''(set_color normal)
     end
 
-    echo -n (set_color 996 --background $fish_color_host_remote)@
+    echo -n (set_color fff --background $fish_color_quote)' '(date '+%H:%M:%S')' '
+    echo -n (set_color --background $fish_color_host_remote $fish_color_quote)' '
     echo -n (set_color fff --background $fish_color_host_remote)(prompt_hostname)' '
     echo -n (set_color --background $fish_color_cwd $fish_color_host_remote)' '
     echo -n (set_color fff --background $fish_color_cwd)(prompt_pwd)' '
@@ -22,16 +33,32 @@ function fish_prompt
         echo -n (set_color $fish_color_quote)"SH:$SHLVL "
     end
 
+    if [ -n "$duration" ]
+        echo -n (set_color normal)$duration' '
+    end
+
+    if test $cmd_status -ne 0
+        echo -n (set_color red)"✘ $cmd_status "
+    end
+
+    echo -n (set_color normal; string trim -l (fish_git_prompt))
+
     set_color normal
     echo
 
-    if [ $cmd_status -eq 0 ]
-        set_color brgreen
-    else
+    set -l symbol '$'
+
+    if [ $cmd_status -ne 0 ]
         set_color $fish_color_status
+    else
+        if fish_is_root_user
+            set_color $fish_color_cwd_root
+            set -l symbol '#'
+        else
+            set_color $fish_color_user
+        end
     end
 
-    echo -n '❯ '
-
+    echo -n $symbol' '
     set_color normal
 end
