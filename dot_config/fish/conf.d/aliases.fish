89e45b30a97fc4d04f34808b7e6e7e66f0a8edb2
Ohad Lutzky <lutzky@google.com>
2024-08-09 17:15:41 +0100

    Use bat -p for cat

--- dot_config/fish/conf.d/aliases.fish
+++ dot_config/fish/conf.d/aliases.fish
@@ -1,11 +1,16 @@
 status is-interactive; or exit 0
+
+# When running bat as cat, use -p to hide line numbers, as I often want to
+# mouse-copy-and-paste from cat output. When running as 'less' though, use all
+# the bells and whistles.
 if type -q /usr/bin/bat
-	alias cat=/usr/bin/bat
+	alias cat='/usr/bin/bat -p'
 	alias less=/usr/bin/bat
 else if type -q /usr/bin/batcat
-	alias cat=/usr/bin/batcat
+	alias cat='/usr/bin/batcat -p'
 	alias less=/usr/bin/batcat
 end
+
 type -q btop && alias top=btop
 type -q fdfind && alias fd=fdfind
 type -q exa && alias ls=exa
