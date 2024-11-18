ecc9a55d81404d361267cdb720fede464ef3e200
Ohad Lutzky <ohad@lutzky.net>
2024-11-18 21:10:46 +0000

    fish: Avoid alerting on interactive commands

--- dot_config/fish/functions/fish_prompt.fish
+++ dot_config/fish/functions/fish_prompt.fish
@@ -17,7 +17,8 @@ function fish_prompt
     set -l human_duration (__human_duration $CMD_DURATION)
 
     if set -q alert_duration_threshold_ms
-        if test $prev_duration -gt $alert_duration_threshold_ms
+        if test $prev_duration -gt $alert_duration_threshold_ms &&
+            not string match -qr $alert_ignore_regex $prev_cmd
             __alert_impl "Long-running command ($human_duration)" \
                 $cmd_status $prev_cmd
         end
