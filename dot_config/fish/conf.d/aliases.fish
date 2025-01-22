c4bda5e0e2968268ab590a3098066d13caeb03d4
Ohad Lutzky <lutzky@google.com>
2025-01-22 11:03:56 +0000

    fish: Fix bat for fzf

--- dot_config/fish/conf.d/aliases.fish
+++ dot_config/fish/conf.d/aliases.fish
@@ -1,4 +1,11 @@
+# Aliases for interactive and non-interactive modes
+if type -q /usr/bin/batcat
+	alias bat='/usr/bin/batcat' # required for fzf preview
+end
+
 status is-interactive; or exit 0
+
+# Aliases for interactive mode only
 
 # When running bat as cat, use -p to hide line numbers, as I often want to
 # mouse-copy-and-paste from cat output. When running as 'less' though, use all
@@ -7,6 +14,7 @@ if type -q /usr/bin/bat
 	alias cat='/usr/bin/bat -p'
 	alias less=/usr/bin/bat
 else if type -q /usr/bin/batcat
+	alias bat='/usr/bin/batcat' # required for fzf preview
 	alias cat='/usr/bin/batcat -p'
 	alias less=/usr/bin/batcat
 end
