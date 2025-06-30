aeac709e03c02bc526342eaa5a5a50d5f8b031d2
Ohad Lutzky <ohad@lutzky.net>
2025-06-30 14:04:32 +0100

    nvim-tree: Enable diagnostics

--- dot_config/nvim/lua/config/nvim-tree.lua
+++ dot_config/nvim/lua/config/nvim-tree.lua
@@ -1,5 +1,10 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+  }
+})
 
 vim.api.nvim_set_keymap('n', '<Leader>tr', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
