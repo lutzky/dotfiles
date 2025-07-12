734b79dc52df0aef93f98cf463fe66c1463d4698
Ohad Lutzky <ohad@lutzky.net>
2025-07-12 19:12:05 +0000

    nvim: Fail gracefully when missing markdown linter

--- dot_config/nvim/after/ftplugin/markdown.lua
+++ dot_config/nvim/after/ftplugin/markdown.lua
@@ -2,6 +2,18 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   callback = function()
     -- try_lint without arguments runs the linters defined in `linters_by_ft`
     -- for the current filetype
-    require("lint").try_lint()
+    if vim.fn.executable('markdownlint-cli2') == 1 then
+      require("lint").try_lint()
+    else
+      _G.markdown_linter_notice_shown = _G.markdown_linter_notice_shown or false
+      if not _G.markdown_linter_notice_shown then
+        vim.notify(
+          'Linter "markdownlint-cli2" not found. Markdown linting is disabled.',
+          vim.log.levels.INFO,
+          {title = 'nvim-lint'}
+        )
+        _G.markdown_linter_notice_shown = true
+      end
+    end
   end,
 })
