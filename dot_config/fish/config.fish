38469164ea7b5ca3fed36436dd4811794545a92b
Ohad Lutzky <ohad@lutzky.net>
2025-08-09 13:08:51 +0100

    alert_ignore_regex: Add cargo watch

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -7,4 +7,18 @@ end
 
 set -g show_duration_threshold_ms 3000
 set -g alert_duration_threshold_ms 30000
-set -g alert_ignore_regex '^n?vim|^tmux|^ssh |^nano |^ncdu|^t |^man |^mutt |^chezmoi cd|^top '
+set -g alert_ignore_regex "\
+^cargo watch |\
+^chezmoi cd|\
+^man |\
+^mutt |\
+^n?vim|\
+^nano |\
+^ncdu|\
+^ssh |\
+^t |\
+^tmux|\
+^top |\
+^\$"
+
+# cSpell: ignore ncdu chezmoi
