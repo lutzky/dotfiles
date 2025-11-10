a43283ed4dccde1f17a20288cebbef6341529404
Ohad Lutzky <ohad@lutzky.net>
2025-11-10 22:35:43 +0000

    nvim: Reintroduce cspell if available

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -1,24 +1,23 @@
-if vim.fn.executable("cspell") == 1 then
-  return {
-    {
-      "nvimtools/none-ls.nvim",
-      event = "VeryLazy",
-      dependencies = { "davidmh/cspell.nvim" },
-      opts = function(_, opts)
-        local cspell = require("cspell")
-        opts.sources = opts.sources or {}
-        table.insert(
-          opts.sources,
-          cspell.diagnostics.with({
-            diagnostics_postprocess = function(diagnostic)
-              diagnostic.severity = vim.diagnostic.severity.HINT
-            end,
-          })
-        )
-        table.insert(opts.sources, cspell.code_actions)
-      end,
-    },
-  }
-else
-  return {}
-end
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    cond = vim.fn.executable("cspell") == 1,
+    dependencies = { "davidmh/cspell.nvim" },
+    lazy = true,
+    keys = { "<leader>s", desc = "Enable spell check" },
+    opts = function(_, opts)
+      local cspell = require("cspell")
+      opts.sources = opts.sources or {}
+      table.insert(
+        opts.sources,
+        cspell.diagnostics.with({
+          diagnostics_postprocess = function(diagnostic)
+            diagnostic.severity = vim.diagnostic.severity.HINT
+          end,
+        })
+      )
+      table.insert(opts.sources, cspell.code_actions)
+    end,
+  },
+}
