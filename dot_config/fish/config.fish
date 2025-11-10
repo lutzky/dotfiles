2831f8bee641f7eeae96d854af2c44f9ec29518a
Ohad Lutzky <ohad@lutzky.net>
2025-11-10 22:05:21 +0000

    fish: Add opencode to path

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
     fish_add_path $HOME/.cargo/bin
     fish_add_path $HOME/.local/bin
     fish_add_path /usr/games # That's where fortune is installed 🤷
+    fish_add_path $HOME/.opencode/bin
 end
 
 set -g show_duration_threshold_ms 3000
