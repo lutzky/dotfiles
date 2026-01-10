2d71c8b86bc2d6cb47642de32b3ac356842d1f36
Ohad Lutzky <ohad@lutzky.net>
2026-01-10 21:13:32 +0000

    nvim: Toggle spelling harder

--- dot_config/nvim/lua/core/keymaps.lua
+++ dot_config/nvim/lua/core/keymaps.lua
@@ -13,6 +13,21 @@ vim.api.nvim_set_keymap("n", "<Leader>w", "<C-w>", {
 
 local function toggle_cspell()
   local is_enabled = vim.lsp.is_enabled("cspell_ls")
+  if not is_enabled then
+    -- mason-lspconfig might not be loaded for this filetype, but user is
+    -- requesting spell-check anyway.
+    require("lazy").load({ wait = true, plugins = { "mason-lspconfig.nvim" } })
+
+    if not (vim.fn.executable("cspell") or vim.fn.executable("npm")) then
+      -- Logic for checking this is usually silent, but user is actively
+      -- requesting this now.
+      vim.api.nvim_echo({
+        { "Probably can't enable spelling: ",     "ErrorMsg" },
+        { "Neither cspell nor npm are available", "WarningMsg" },
+      }, true, {})
+    end
+  end
+
   vim.lsp.enable("cspell_ls", not is_enabled)
 end
 
