77a796c82f413c0ffb13417922217efcd9642b32
Ohad Lutzky <ohad@lutzky.net>
2026-01-19 17:09:26 +0000

    kitty: Remove open_as_url debug log

--- dot_config/kitty/executable_open_as_url.sh
+++ dot_config/kitty/executable_open_as_url.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-echo "open_as_url $1" >> ~/open_as_url.log
-
 if [[ $OSTYPE == "darwin"* ]]; then
 	OPEN_CMD="open"
 else
 	OPEN_CMD="xdg-open"
 fi
 
-echo "opening" >> ~/open_as_url.log
-
-$OPEN_CMD "http://$1" >> ~/open_as_url.log 2>&1
+$OPEN_CMD "http://$1"
