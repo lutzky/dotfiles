76587a94f1fb23248b6619e3e4387feed0cb5126
Ohad Lutzky <ohad@lutzky.net>
2026-01-29 22:31:51 +0000

    nvim: Add LSP codelens support

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,3 +1,25 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local bufnr = args.buf
+
+    if client and client.supports_method("textDocument/codeLens") then
+      vim.api.nvim_create_autocmd({ "CursorHold", "InsertLeave" }, {
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.codelens.refresh({ bufnr = bufnr })
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>cr", vim.lsp.codelens.run, {
+        buffer = bufnr,
+        desc = "LSP CodeLens Run"
+      })
+    end
+  end,
+})
+
 return {
   "neovim/nvim-lspconfig",
   lazy = true,
