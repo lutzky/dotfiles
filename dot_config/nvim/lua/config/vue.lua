3393c5ab6ceb8dd2db2b4f9613bcbc8ac5f1eb9b
Ohad Lutzky <ohad@lutzky.net>
2026-02-25 21:25:22 +0000

    nvim: Lint

--- dot_config/nvim/lua/config/vue.lua
+++ dot_config/nvim/lua/config/vue.lua
@@ -6,7 +6,8 @@ end
 --
 -- Source below is adapted from https://github.com/vuejs/language-tools/wiki/Neovim
 
-local vue_language_server_path = vim.fn.stdpath('data') .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+local vue_language_server_path = vim.fn.stdpath('data') ..
+    "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
 local vue_plugin = {
   name = '@vue/typescript-plugin',
@@ -50,13 +51,14 @@ local vue_ls_config = {
       vim.list_extend(clients, vtsls_clients)
 
       if #clients == 0 then
-        vim.notify('Could not find `vtsls` or `ts_ls` lsp client, `vue_ls` would not work without it.', vim.log.levels.ERROR)
+        vim.notify('Could not find `vtsls` or `ts_ls` lsp client, `vue_ls` would not work without it.',
+          vim.log.levels.ERROR)
         return
       end
       local ts_client = clients[1]
 
-      local param = unpack(result)
-      local id, command, payload = unpack(param)
+      local param = table.unpack(result)
+      local id, command, payload = table.unpack(param)
       ts_client:exec_cmd({
         title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
         command = 'typescript.tsserverRequest',
@@ -65,14 +67,14 @@ local vue_ls_config = {
           payload,
         },
       }, { bufnr = context.bufnr }, function(_, r)
-          local response = r and r.body
-          -- TODO: handle error or response nil here, e.g. logging
-          -- NOTE: Do NOT return if there's an error or no response, just return nil back to the vue_ls to prevent memory leak
-          local response_data = { { id, response } }
+        local response = r and r.body
+        -- TODO: handle error or response nil here, e.g. logging
+        -- NOTE: Do NOT return if there's an error or no response, just return nil back to the vue_ls to prevent memory leak
+        local response_data = { { id, response } }
 
-          ---@diagnostic disable-next-line: param-type-mismatch
-          client:notify('tsserver/response', response_data)
-        end)
+        ---@diagnostic disable-next-line: param-type-mismatch
+        client:notify('tsserver/response', response_data)
+      end)
     end
   end,
 }
@@ -80,4 +82,4 @@ local vue_ls_config = {
 vim.lsp.config('vtsls', vtsls_config)
 vim.lsp.config('vue_ls', vue_ls_config)
 vim.lsp.config('ts_ls', ts_ls_config)
-vim.lsp.enable({'ts_ls', 'vue_ls'}) -- If using `ts_ls` replace `vtsls` to `ts_ls`
+vim.lsp.enable({ 'ts_ls', 'vue_ls' }) -- If using `ts_ls` replace `vtsls` to `ts_ls`
