b624cf1fd29a1eb5361b2d7ecb50e38edf4fa978
Ohad Lutzky <ohad@lutzky.net>
2026-03-02 15:32:51 +0000

    nvim: Markdown checkbox toggling

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,15 +1,28 @@
 return {
-  'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {
-    completions = {
-      lsp = { enabled = true },
-    },
-    checkbox = {
-      checked = {
-        scope_highlight = '@markup.strikethrough' }
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      completions = {
+        lsp = { enabled = true },
+      },
+      checkbox = {
+        checked = {
+          scope_highlight = '@markup.strikethrough' }
+      },
     },
   },
+  {
+    'opdavies/toggle-checkbox.nvim', -- cspell: disable-line
+    keys = {
+      {
+        "<leader>tt",
+        function()
+          require('toggle-checkbox').toggle()
+        end,
+        desc = "Toggle checkbox on current line"
+      } },
+  }
 }
