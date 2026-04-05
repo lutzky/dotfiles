741b6cf3b211b59009f0f5f261d89728aaf844cd
Ohad Lutzky <ohad@lutzky.net>
2026-04-05 20:23:28 +0100

    nvim: Stop using relativenumber

--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -14,5 +14,5 @@ vim.opt.laststatus = 2
 
 -- Line numbers
 vim.opt.number = true
-vim.opt.relativenumber = true
+--vim.opt.relativenumber = true -- I'm likelier to need the line number than a count
 vim.opt.cursorline = true
